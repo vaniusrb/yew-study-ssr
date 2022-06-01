a7708f0d8ab15e88817d4ee06901db9aa7323650
vaniusrb <vanius@gmail.com>
2022-06-01 18:14:55 -0300

    Little changes

--- src/bin/simple_ssr_server.rs
+++ src/bin/simple_ssr_server.rs
@@ -1,9 +1,20 @@
+use axum::body::Body;
+use axum::error_handling::HandleError;
+use axum::extract::Query;
+use axum::handler::Handler;
+use axum::http::Request;
+use axum::http::StatusCode;
+use axum::response::Html;
+use axum::routing::get;
+use axum::{Extension, Router, Server};
 use clap::Parser;
 use once_cell::sync::Lazy;
 use simple_ssr::app_api::AppApi;
+use std::collections::HashMap;
 use std::path::PathBuf;
 use tokio_util::task::LocalPoolHandle;
-use warp::Filter;
+use tower::ServiceExt;
+use tower_http::services::ServeDir;
 
 // We spawn a local pool that is as big as the number of cpu threads.
 static LOCAL_POOL: Lazy<LocalPoolHandle> = Lazy::new(|| LocalPoolHandle::new(num_cpus::get()));
@@ -12,14 +23,27 @@ static LOCAL_POOL: Lazy<LocalPoolHandle> = Lazy::new(|| LocalPoolHandle::new(num
 #[derive(Parser, Debug)]
 struct Opt {
     /// the "dist" created by trunk directory to be served for hydration.
-    #[structopt(short, long, parse(from_os_str))]
+    #[clap(short, long, parse(from_os_str))]
     dir: PathBuf,
 }
 
-async fn render(index_html_s: &str) -> String {
+async fn render(
+    Extension(index_content): Extension<IndexContent>,
+    url: Request<Body>,
+    Query(queries): Query<HashMap<String, String>>,
+) -> Html<String> {
+    // let url = url.uri().to_string();
+
     let content = LOCAL_POOL
         .spawn_pinned(move || async move {
+            // let server_app_props = ServerAppProps {
+            //     url: url.into(),
+            //     queries,
+            // };
+
             let renderer = yew::ServerRenderer::<AppApi>::new();
+
+            // ::with_props(server_app_props);
 
             renderer.render().await
         })
@@ -28,26 +52,56 @@ async fn render(index_html_s: &str) -> String {
 
     // Good enough for an example, but developers should avoid the replace and extra allocation
     // here in an actual app.
-    index_html_s.replace("<body>", &format!("<body>{}", content))
+    Html(
+        index_content
+            .0
+            .replace("<body>", &format!("<body>{}", content)),
+    )
 }
+
+#[derive(Clone)]
+struct IndexContent(String);
 
 #[tokio::main]
 async fn main() {
+    // env_logger::init();
+
     let opts = Opt::parse();
 
     let index_html_s = tokio::fs::read_to_string(opts.dir.join("index.html"))
         .await
         .expect("failed to read index.html");
 
-    let html = warp::path::end().then(move || {
-        let index_html_s = index_html_s.clone();
+    let handle_error = |e| async move {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("error occurred: {}", e),
+        )
+    };
 
-        async move { warp::reply::html(render(&index_html_s).await) }
-    });
+    let index_content = IndexContent(index_html_s.clone());
 
-    let routes = html.or(warp::fs::dir(opts.dir));
+    let app = Router::new()
+        .route("/api/test", get(|| async move { "Hello World" }))
+        // needed because https://github.com/tower-rs/tower-http/issues/262
+        .route("/", get(render))
+        .layer(Extension(index_content.clone()))
+        .fallback(HandleError::new(
+            ServeDir::new(opts.dir)
+                .append_index_html_on_directories(false)
+                .fallback(
+                    render
+                        .layer(Extension(index_content))
+                        .into_service()
+                        .map_err(|err| -> std::io::Error { match err {} }),
+                ),
+            handle_error,
+        ));
 
     println!("You can view the website at: http://localhost:8080/");
 
-    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
+    Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
